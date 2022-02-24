570e28e7cef357160b7a8a7f6cbfbd631cdcfe56
bahau2411 <98308345+bahau2411@users.noreply.github.com>
2022-02-24 10:41:06 +0700

    Add files via upload

---
+++ Exercises/CalculatorApplication/LoginAccount/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculatorApplication
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            listBox1.Items.Add("SEMESTER 5");
+            listBox1.Items.Add("- PRN211");
+            listBox1.Items.Add("- SWP311");
+            listBox1.Items.Add("- SWR201");
+            listBox1.Items.Add("- SWT301");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
